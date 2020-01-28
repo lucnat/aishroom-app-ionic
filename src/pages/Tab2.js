fb6ff8fe5761f79d0df3edc7c196d409a9b7a39d
Luca Naterop <luca@Lucas-MacBook-Pro.local>
2020-01-28 23:41:09 +0100

    add wikipedia

--- src/pages/Tab2.js
+++ src/pages/Tab2.js
@@ -14,6 +14,11 @@ class Tab2 extends React.Component {
         <IonContent>
           <div style={{padding: 15}}>
             <h1>Tab 2</h1>
+
+          <iframe src="https://en.wikipedia.org/wiki/Special:Random" title="Random Wikipedia Article" width="100%" height="300" style={{border: 0}}>
+            <p>Your browser does not support iframes.</p>
+          </iframe>
+
           </div>
         </IonContent>
       </IonPage>
